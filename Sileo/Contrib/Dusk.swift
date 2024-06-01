e0d5f6254427908854169ca9fb582ceb9029a57f
plplpmmmmh <147246648+plplpmmmmh@users.noreply.github.com>
2024-06-01 11:09:56 +0800

    Update Dusk.swift

--- Sileo/Contrib/Dusk.swift
+++ Sileo/Contrib/Dusk.swift
@@ -12,6 +12,6 @@ class Dusk {
     static let backgroundColor = UIColor(red: 30.0/255.0, green: 32.0/255.0, blue: 40.0, alpha: 1)
     static let foregroundColor = UIColor.white
     static let warningColor = UIColor(red: 1, green: 231.0/255.0, blue: 146.0/255.0, alpha: 1)
-    static let errorColor = UIColor(red: 219.0/255.0, green: 44.0/255.0, blue: 56.0/255.0, alpha: 1)
+    static let errorColor = UIColor(red: 219.0/255.0, green: 135.0/255.0, blue: 135.0/255.0, alpha: 1)
     static let debugColor = UIColor(red: 0, green: 160.0/255.0, blue: 190.0/255.0, alpha: 1)
 }
