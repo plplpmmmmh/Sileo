6fc7dbd64c1c529c4d17e6b292dd8ad643dba539
plplpmmmmh <147246648+plplpmmmmh@users.noreply.github.com>
2024-06-02 07:05:04 +0800

    Update DownloadsTableViewCell.swift

--- Sileo/UI/DownloadsViewController/DownloadCell/DownloadsTableViewCell.swift
+++ Sileo/UI/DownloadsViewController/DownloadCell/DownloadsTableViewCell.swift
@@ -8,6 +8,27 @@
 
 import Foundation
 import Evander
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
 
 class DownloadsTableViewCell: BaseSubtitleTableViewCell {
     public var package: DownloadPackage? = nil {
@@ -93,7 +114,7 @@ class DownloadsTableViewCell: BaseSubtitleTableViewCell {
             if errored {
                 download = nil
             }
-            self.textLabel?.textColor = errored ? .red : .sileoLabel
+            self.textLabel?.textColor = errored ? UIColor(hex: "c7918f") : .sileoLabel
             self.detailTextLabel?.textColor = errored ? .red : UIColor(red: 172.0/255.0, green: 184.0/255.0, blue: 193.0/255.0, alpha: 1)
         }
     }
